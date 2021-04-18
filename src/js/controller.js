b50273f6edb9fd4a2203053e398b31b43e9eacce
Joyce <joyterra@gmail.com>
2021-04-18 17:48:27 +0100

    Implement basic styling

---
+++ src/js/controller.js
@@ -0,0 +1,20 @@
+const cardContent = [
+    { id: 01,
+        title: "Votre studio",
+        price: "390",
+    },
+    { id: 02,
+        title: "Votre 2 piéces",
+        price: "570", 
+    },
+    { id: 03,
+        title: "Votre 3 piéces",
+        price: "814",
+    },
+    { id: 04,
+        title: "Votre 4 piéces",
+        price: "972",
+    } 
+]
+
+

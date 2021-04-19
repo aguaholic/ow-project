67f0f4f00e156d189e26edf77994d6113e454838
Joyce <joyterra@gmail.com>
2021-04-19 14:34:01 +0100

    Implement and styel cards

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,19 +1,28 @@
 const cardContent = [
-    { id: 01,
+    {   
+        id: 01,
         title: "Votre studio",
         price: "390",
+        class: "card-1",
     },
-    { id: 02,
+    {   
+        id: 02,
         title: "Votre 2 piéces",
-        price: "570", 
+        price: "570",
+        class: "card-2",
+
     },
-    { id: 03,
+    {   
+        id: 03,
         title: "Votre 3 piéces",
         price: "814",
+        class: "card-3",
     },
-    { id: 04,
+    {   
+        id: 04,
         title: "Votre 4 piéces",
         price: "972",
+        class: "card-4",
     } 
 ]
 
@@ -23,12 +32,11 @@ const cardTemplate = document.querySelector('#card-template')
 if(cardsContainer) {
     cardContent.forEach(cardData => {
         const card = cardTemplate.content.cloneNode(true)
-
+        
+        card.querySelector('.card').classList.add(cardData.class) 
         card.querySelector('.title').textContent = cardData.title
         card.querySelector('.amount').textContent = `${cardData.price}€`
 
         cardsContainer.appendChild(card)
     })
 }
-
-console.log('blaaaa')
